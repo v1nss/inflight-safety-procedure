858364416afbcc6bd3ef72c4b197730e053e1d8d
v1nss <vincemarc.mr@gmail.com>
2025-09-10 16:08:40 +0800

    Practicum Menu UI Scene transitions setup

--- Assets/Scripts/Practicum/PracticumMenu.cs
+++ Assets/Scripts/Practicum/PracticumMenu.cs
@@ -1,6 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PracticumMenu
-{
-    
+public class PracticumMenu : MonoBehaviour {
+
+    [Header("Practicum Menu Buttons")]
+    public Button airmaskButton;
+    public Button seatbeltButton;
+    public Button lifevestButton;
+    public Button airSackButton;
+    public Button exitDoorsButton;
+    public Button quitButton;
+
+    void Start()
+    {
+        airmaskButton.onClick.AddListener(StartAirMask);
+        seatbeltButton.onClick.AddListener(StartSeatbelt);
+        lifevestButton.onClick.AddListener(StartLifevest);
+        airSackButton.onClick.AddListener(StartAirSack);
+        exitDoorsButton.onClick.AddListener(StartExitDoors);
+        quitButton.onClick.AddListener(QuitPracticum);
+    }
+
+    public void StartAirMask()
+    {
+
+    }
+
+    public void StartSeatbelt()
+    {
+
+    }
+
+    public void StartLifevest()
+    {
+        SceneTransitionManager.singleton.GoToSceneAsync(2);
+    }
+
+    public void StartAirSack()
+    {
+
+    }
+
+    public void StartExitDoors()
+    {
+
+    }
+
+    public void QuitPracticum()
+    {
+        SceneTransitionManager.singleton.GoToSceneAsync(0);
+    }
+
 }
